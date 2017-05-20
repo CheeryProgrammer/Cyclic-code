8f0254a152efde360172594dd6a743e89ef5fc9e
Sergey Voroshilin <medulla_261@mail.ru>
2017-05-20 15:53:59 +0300

    Add project files.

---
+++ CyclicCode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_4
+{
+	public partial class CyclicCode : Form
+	{
+		private int _n;
+		private int _k;
+		private int[] _generationPol;
+		private int[] _checkingPol;
+
+		public CyclicCode()
+		{
+			InitializeComponent();
+		}
+
+		private void BtnEncode_Click(object sender, EventArgs e)
+		{
+			ReadInput();
+			CalculateCheckingPolynomial();
+			CalculateCheckingCoefficients();
+		}
+	}
+}
